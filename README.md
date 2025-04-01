1fa504f0f6a3c8556ef8e547a63f20df151b1918
Gustavo Umbelino <gumbelin@gmail.com>
2025-04-01 11:15:27 +0200

    added setup script

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # Generating LLM data
 
 Requirements:
-- [ollama](https://ollama.com/) 
+
+- [homebrew](https://brew.sh/)
+- [git](https://git-scm.com/)
+- [ollama](https://ollama.com/)
+
+## Getting started
+
+Step 1. Install homebrew
+
+Step 2. Install python
+
+brew install python
+
+Step 3. Install git
+
+brew install git
+
+Step 4. Clone git repo
+
+git clone https://github.com/gumbelino/drillm
+
+Follow instructions
+
+Install git
+Install python
+Install pipenv
--- test.R
+++ test.R
@@ -45,13 +45,19 @@ process_excel_file <- function(file_path) {
     cat("Number of considerations:", n_c, "\n")
     cat("Number of policies:", n_p, "\n")
     cat("Integer value from 'scale_max' column:", scale_max, "\n")
-    cat("Logical value from 'q-method' column:", q_method, "\n\n")
+    cat("Logical value from 'q-method' column:", q_method, "\n")
     cat(rep("-", 40), "\n")
     
     
     ### Make random survey
-    # Generate data for C1:C50 with Likert scale values (1 to 7) randomly assigned
-    c_df <- data.frame(t(replicate(n_c, sample(1:scale_max, 1, replace = TRUE))))
+    # Generate data for C1:C50 with Likert scale values [1, scale_max] randomly assigned
+    if (q_method) {
+      c_df <- data.frame(t(round(rnorm(n = n_c, mean = scale_max / 2, sd = scale_max / 4))))
+    } else {
+      c_df <- data.frame(t(replicate(n_c, sample(1:scale_max, 1, replace = TRUE))))
+    }
+    
+    # fill and rename columns
     c_df[1, (n_c + 1):50] <- NA
     colnames(c_df) <- paste0("C", 1:50)
     
@@ -67,11 +73,28 @@ process_excel_file <- function(file_path) {
       mutate(survey = sheet_name) %>%
       relocate(survey, .before = 1)
     
+    cat(dataset)
+    
   }
 }
 
 # Example usage
 process_excel_file(SURVEY_FILE)
+
+seed(123)
+
+
+
+sample_values[sample_values < 0] <- NA
+sample_values[is.na(sample_values)] <- sample(0:11, sum(is.na(sample_values)), replace = TRUE)
+sample_values
+
+sampled_values <- rnorm(n = 5, mean = (0 + 11) / 2, sd = (11 - 0) / 4)
+sampled_values[sampled_values < 0] <- 0
+sampled_values[sampled_values > 11] <- 11
+sampled_values
+
+floor(rnorm(5) + 5.5)
 
   
   
