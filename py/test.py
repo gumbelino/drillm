f56294435eef541a7f392d813a249e3cb8d9d56f
Gustavo Umbelino <gumbelin@gmail.com>
2025-04-11 11:40:39 +0200

    updated model info

--- py/test.py
+++ py/test.py
@@ -4,4 +4,7 @@ co = cohere.Client()
 response = co.models.list()
 
 for model in response.models:
-    print(model.name)
+    if "command" in model.name:
+        print(f"\n====== {model.name} ======")
+        print(f"\tContext length: {model.context_length}")
+        print(f"\tFine-tunned: {model.finetuned}")
