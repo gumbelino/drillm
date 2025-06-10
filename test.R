7d7d67230afbdcf8367b42a4b9ef3695ed2febc6
Gustavo Umbelino <gumbelin@gmail.com>
2025-06-10 19:25:20 +0200

    added permutation test results (human-only)

--- test.R
+++ test.R
@@ -1,10 +1,16 @@
-# Create a sample data frame with one row of 50 random numbers between 1 and 7
-set.seed(123) # For reproducibility
-data <- tibble(C1 = sample(1:7, 50, replace = TRUE))
 
-# Plot the histogram for the distribution of numbers in this row
-ggplot(data, aes(x = C1)) +
-  geom_histogram(bins = 7, fill = "blue", color = "black") +
-  labs(title = "Distribution of Numbers from 1 to 7",
-       x = "Number",
-       y = "Frequency")
+data <- tibble(
+  C1 = c(1,2,3,4),
+  C2 = c(1,2,3,4),
+  C3 = c(1,2,3,4),
+  P1 = c(1,2,3,4),
+  P2 = c(1,2,3,4),
+  P3 = c(1,2,3,4),
+  PId = c(1,2,3,4),
+)
+
+# shuffle the dataframe by rows
+pref_cols <- grep("^P\\d", names(data), value = TRUE)
+shuffled_data <- data
+shuffled_data[pref_cols] <- shuffled_data[sample(1:nrow(shuffled_data)), pref_cols]
+
